ebbc220c198652a96ba8d2696d2dd70b8ee352bd
Tomer Cahal <tomercahal@gmail.com>
2019-11-15 19:58:39 +0200

    The logical part is completed, now I have to add the image of the DVD logo

--- DVD_Simulation.py
+++ DVD_Simulation.py
@@ -1,9 +1,9 @@
 import pygame
-import random  # Used for when choosing starting movement for ball
-import time
-import math
+import random  # Used for when choosing starting movement for logo
+import os
 
 # Constants
+DVD_LOGO = 'DVD logo.png'  # This is the dvd logo that will move around.
 WINDOW_WIDTH = 1000  # X
 WINDOW_HEIGHT = 800  # Y
 REFRESH_RATE = 60
@@ -11,34 +11,36 @@ finish = False  # Used for knowing when the user wants to end the game
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
-RADIUS = 5
+len_x = 178  # Length of the actual logo in pixels
+len_y = 82  # Length of the actual logo in pixels
+HOW_FAST = 3  # Used to determine how fast the logo will go
 
 
-def case_1(y_pos):  # Checking if the ball hit the bottom floor(need to increase Y)
-    return y_pos + RADIUS >= WINDOW_HEIGHT
+def case_1(y_pos):  # Checking if the logo hit the bottom floor(need to increase Y)
+    return y_pos + len_y >= WINDOW_HEIGHT
 
 
-def case_2(y_pos):  # Checking if the ball hit the top floor(need to decrease Y)
-    return y_pos - RADIUS <= 0
+def case_2(y_pos):  # Checking if the logo hit the top floor(need to decrease Y)
+    return y_pos - len_y <= 0
 
 
-def case_3(x_pos):  # Checking if the ball hit the right side(need to decrease X)
-    return x_pos + RADIUS >= WINDOW_WIDTH
+def case_3(x_pos):  # Checking if the logo hit the right side(need to decrease X)
+    return x_pos + len_x >= WINDOW_WIDTH
 
 
-def case_4(x_pos):  # Checking if the ball hit the left side(need to decrease X)
-    return x_pos - RADIUS <= 0
+def case_HOW_FAST(x_pos):  # Checking if the logo hit the left side(need to decrease X)
+    return x_pos - len_x <= 0
 
 
 def choose_rand(start_value=0, end_value=0):
     """This function creates a random number that is either -1 or 1 if no params were given,
     if params were given it will return a random between the ones given"""
-    if start_value and end_value !=0:
+    if start_value and end_value != 0:
         return random.randint(start_value, end_value)
     if random.random() < 0.5:
-        return 1
+        return HOW_FAST
     else:
-        return -1
+        return -HOW_FAST
 
 
 # Init screen
@@ -47,32 +49,33 @@ size = (WINDOW_WIDTH, WINDOW_HEIGHT)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("DVD Simulation")
 clock = pygame.time.Clock()
-ball_x_pos = choose_rand(1, WINDOW_WIDTH)  # Chooses a random value for X to start from
-ball_y_pos = choose_rand(1, WINDOW_HEIGHT)  # chooses a random value for Y to start from
+logo = pygame.image.load(DVD_LOGO)  # This loads the dvd logo into the variable logo
+logo_x_pos = choose_rand(1, WINDOW_WIDTH)  # Chooses a random value for X to start from
+logo_y_pos = choose_rand(1, WINDOW_HEIGHT)  # chooses a random value for Y to start from
 X_CHANGER = choose_rand()  # Automatically gives them either 1 or -1
 Y_CHANGER = choose_rand()  # Automatically gives them either 1 or -1
 pygame.display.flip()
 
 while not finish:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:  # Checking if the user has pressed the quit quit button on the application
             finish = True
             pygame.quit()
+            os._exit(1)  # Terminates the program
     screen.fill(BLACK)
-    ball_x_pos = ball_x_pos + X_CHANGER
-    ball_y_pos = ball_y_pos + Y_CHANGER
-    if not ball_y_pos + RADIUS in range(1,WINDOW_HEIGHT) or not ball_y_pos - RADIUS in range(1,WINDOW_HEIGHT)\
-            or not ball_x_pos + RADIUS in range(1,WINDOW_WIDTH) or not ball_x_pos - RADIUS in range(1,WINDOW_WIDTH):
-        # This big if checks if the ball has touched one of the sides
-        if case_1(ball_y_pos):  # Ball it hitting the bottom floor
-            Y_CHANGER = -1
-        if case_2(ball_y_pos):  # Ball is hitting the top floor
-            Y_CHANGER = 1
-        if case_3(ball_x_pos):  # Ball is hitting the right side
-            X_CHANGER = -1
-        if case_4(ball_x_pos):  # Ball is hitting the left side
-            X_CHANGER = 1
-    pygame.draw.circle(screen, WHITE, [ball_x_pos, ball_y_pos], RADIUS)
+    logo_x_pos = logo_x_pos + X_CHANGER
+    logo_y_pos = logo_y_pos + Y_CHANGER
+    if not logo_y_pos + len_y in range(1, WINDOW_HEIGHT) or not logo_y_pos in range(1, WINDOW_HEIGHT) \
+            or not logo_x_pos + len_x in range(1, WINDOW_WIDTH) or not logo_x_pos in range(1, WINDOW_WIDTH):
+        # This big if checks if the logo has touched one of the sides
+        if case_1(logo_y_pos):  # logo it hitting the bottom floor
+            Y_CHANGER = -HOW_FAST
+        if case_2(logo_y_pos):  # logo is hitting the top floor
+            Y_CHANGER = HOW_FAST
+        if case_3(logo_x_pos):  # logo is hitting the right side
+            X_CHANGER = -HOW_FAST
+        if case_HOW_FAST(logo_x_pos):  # logo is hitting the left side
+            X_CHANGER = HOW_FAST
+    screen.blit(logo, (logo_x_pos, logo_y_pos))  # printing the new logo
     pygame.display.flip()
     clock.tick(REFRESH_RATE)
-
